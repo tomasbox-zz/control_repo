6cfe87fdd576ed4950fa9392214608b78c44b026
tomasbox <31577628+tomasbox@users.noreply.github.com>
2020-08-31 11:23:53 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,10 +7,6 @@ node default {
 }
 node 'master.puppet.vm' {
    include role::master_server
-   file {'/root/README':
-     ensure  => file,
-     content => "Welcome to ${fqdn}\n",
-   }
 }
 
 node /^web/ {
