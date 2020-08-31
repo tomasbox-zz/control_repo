22d73532fa36901fb756cc15f661f20f740ecf06
tomasbox <31577628+tomasbox@users.noreply.github.com>
2020-08-31 10:58:05 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCt9jLRmyU9dndLlZpX/bKcXC1cB8xk2c/0On26TXBUMii6aJdaddzB//k4Yy7E//5cBMgmcyFgQ+QAA8kVU6epmKFal1ThbaN7Q7unQuviNgfkYMT1NkQfunwi620pt4naukDMkMx6Bv/X8pE50iIOeyMacTcd2+9Otz/ozbj8dbiZ/tWPIi2KwataTgMaRhTpqFDy2hwZv0e1FjE52Wd/jgm1TZsSNGVMORh+EsrVPCBWZ8DOheonzCXVFlDe+p3mvCkyjZrC5Hu4ZCnXMqdFVNIHIDSmnNquEC4eexfCdT45CoflBnIsorXbPZ/TOzwKg90dcwYq8wcriPiiqSR5',
   }  
 }
